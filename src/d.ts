a5510073fb1bad7fb4771ed14c357428a7f5dc22
Isac Fadoni <isacfadoni@gmail.com>
2021-02-24 23:53:35 -0300

    feat(hooks/use-controls): Internal hook for exposing controlling functions.

---
+++ src/d.ts
@@ -0,0 +1,25 @@
+export interface ICarouselControls {
+  next(): void
+  previous(): void
+  restart(): void
+  goTo(where: number): void
+}
+
+export interface ICarouselControlsHookOptions {
+  isLoopEnabled: boolean
+  isLastCarouselPosition: boolean
+}
+
+export interface ICarouselHook extends ICarouselControls {
+  carouselItem: any
+  carouselPosition: number
+  carouselLength: number
+}
+
+export interface ICarouselHookOptions {
+  controls: {
+    autoPlay: boolean
+    loop: boolean
+    intervalInMs: number
+  }
+}
