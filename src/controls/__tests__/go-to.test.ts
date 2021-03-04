f03489d169e3458e0fdfdd0062de5d49c5803617
Isac Fadoni <isacfadoni@gmail.com>
2021-03-04 19:21:16 -0300

    tests(controls/goto): Improved readability.

--- src/controls/__tests__/go-to.test.ts
+++ src/controls/__tests__/go-to.test.ts
@@ -3,17 +3,23 @@ import { goTo } from '../go-to'
 describe('Control: Go To', () => {
   describe('Moving to desired position', () => {
     test('returns the desired position', () => {
-      expect(goTo(0, 1, 5)).toBe(1)
+      const [currentPosition, desiredPosition, contentLength] = [0, 1, 5]
+
+      expect(goTo(currentPosition, desiredPosition, contentLength)).toBe(1)
     })
   })
 
   describe('When desired position is out limit', () => {
     test('returns the current position when desired was less than zero', () => {
-      expect(goTo(0, -1, 5)).toBe(0)
+      const [currentPosition, desiredPosition, contentLength] = [0, -1, 5]
+
+      expect(goTo(currentPosition, desiredPosition, contentLength)).toBe(0)
     })
 
     test('returns the current position when desired was greater than length', () => {
-      expect(goTo(0, 5, 5)).toBe(0)
+      const [currentPosition, desiredPosition, contentLength] = [0, 5, 5]
+
+      expect(goTo(currentPosition, desiredPosition, contentLength)).toBe(0)
     })
   })
 })
