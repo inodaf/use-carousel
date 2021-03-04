d6965143b02782446857e21dfc85ca608ce3d740
Isac Fadoni <isacfadoni@gmail.com>
2021-03-04 19:22:45 -0300

    feat(carousel): Connecting controls and features.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import { useState, useCallback } from 'react'
+import { useState, useEffect, Dispatch } from 'react'
+import { ICarouselHook, ICarouselHookOptions } from './d'
+import { useControls } from './hooks/use-controls'
+import { useFeatures } from './hooks/use-features'
 
-export default function useCounter(): { count: number; increment: () => any } {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => setCount((x) => x + 1), [])
-  return { count, increment }
+export function useCarousel<T>(
+  content: T[],
+  options: ICarouselHookOptions
+): ICarouselHook {
+  const [carouselPosition, setCarouselPosition] = useState(0)
+  const [carouselItem, setCarouselItem]: [T, Dispatch<T>] = useState(null)
+
+  const carouselLength = content.length
+  const isLastCarouselPosition = carouselPosition === carouselLength - 1
+
+  const isAutoPlayEnabled = Boolean(options.controls.autoPlay)
+  const isLoopEnabled = Boolean(options.controls.loop)
+
+  const controls = useControls(
+    carouselPosition,
+    carouselLength,
+    setCarouselPosition,
+    { isLoopEnabled, isLastCarouselPosition }
+  )
+
+  const features = useFeatures(controls, options, {
+    isLastCarouselPosition,
+  })
+
+  useEffect(() => setCarouselItem(content[carouselPosition]), [
+    content,
+    carouselPosition,
+  ])
+
+  useEffect(() => {
+    if (isAutoPlayEnabled) features.autoPlay()
+    return () => features.stopAutoPlay()
+  }, [isAutoPlayEnabled, features])
+
+  return {
+    carouselItem,
+    carouselLength,
+    carouselPosition,
+    ...controls,
+  }
 }
